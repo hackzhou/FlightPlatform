1fab5403e204a8f6c7a2b291d22bdc2dcddd2e0d
zhouzhou <zhouzhou@192.168.31.243>
2017-04-09 09:07:36 +0800

    FlightPlatform Maven Project

---
+++ src/main/java/com/ly/flight/FlightPlatform/App.java
@@ -0,0 +1,13 @@
+package com.ly.flight.FlightPlatform;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
