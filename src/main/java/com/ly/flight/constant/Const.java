5c06d78590cfbb0910add57c183e7f27eeb0d6de
zhouzhou <zhouzhou@192.168.31.243>
2017-04-09 18:20:44 +0800

    Update pom.xml

---
+++ src/main/java/com/ly/flight/constant/Const.java
@@ -0,0 +1,7 @@
+package com.ly.flight.constant;
+
+public class Const {
+	
+	public static final String CONST_TEST = "CONST_TEST";
+
+}
---
+++ src/test/java/com/ly/flight/MyTest.java
@@ -0,0 +1,12 @@
+package com.ly.flight;
+
+import org.junit.Test;
+
+public class MyTest {
+	
+	@Test
+	public void test001(){
+		System.out.println("zhouzhou test ok !");
+	}
+
+}
