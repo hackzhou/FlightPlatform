a40477556095e3ad09d7df9227f23104dba48a40
Administrator <Administrator@20170419-141915>
2017-04-25 17:06:01 +0800

    Update BaseDao

--- src/main/java/com/ly/flight/common/dao/IBaseDao.java
+++ src/main/java/com/ly/flight/common/dao/IBaseDao.java
@@ -10,19 +10,19 @@ public interface IBaseDao {
 	public Object get(Class<?> pojoClass, Serializable id);
 	public void save(Object pojo) throws Exception;
 	public void update(Object pojo) throws Exception;
-	public void delete(Object pojo) throws Exception;
 	public void insertOrUpdate(Object pojo) throws Exception;
+	public void delete(Object pojo) throws Exception;
 	public void delete(Class<?> pojoClass, Serializable id) throws Exception;
+	public Object findOne(String hql, Object[] param);
 	public List<?> find(String hql);
 	public List<?> find(String hql, Object[] param);
-	public Object findOne(String hql, Object[] param);
 	public List<?> find(String queryString, Object[] param, boolean userCache);
 	public List<?> find(String hql, Object[] params, int firstResult, int maxResult);
 	public List<?> find(String hql, Object[] params, int firstResult, int maxResult, boolean userCache);
-	public int executeByHql(String hql);
-	public int executeByHql(String hql,Object[] param);
 	public List<?> queryBySql(String sql);
-	public Iterator<?> iterate(String hql, Object[] objects);
+	public int executeByHql(String hql);
+	public int executeByHql(String hql, Object[] param);
 	public Iterator<?> iterate(String hql);
+	public Iterator<?> iterate(String hql, Object[] objects);
 
 }
--- src/main/java/com/ly/flight/common/dao/impl/BaseDao.java
+++ src/main/java/com/ly/flight/common/dao/impl/BaseDao.java
@@ -1,108 +1,54 @@
 package com.ly.flight.common.dao.impl;
 
 import java.io.Serializable;
-import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.List;
-import org.hibernate.HibernateException;
-import org.hibernate.Query;
+import org.hibernate.query.Query;
 import org.hibernate.Session;
-import org.springframework.orm.hibernate3.HibernateCallback;
-import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 import com.ly.flight.common.dao.IBaseDao;
 
-@SuppressWarnings("deprecation")
 public class BaseDao extends HibernateDaoSupport implements IBaseDao {
 
+	@Override
 	public Object load(Class<?> pojoClass, Serializable id) {
-		return getHibernateTemplate().load(pojoClass, id);
+		return this.getHibernateTemplate().load(pojoClass, id);
 	}
 	
-	public void save(Object pojo) throws Exception {
-		getHibernateTemplate().save(pojo);
-	}
-	
-	public List<?> queryBySql(String sql) {
-		List<?> l = getHibernateTemplate().find(sql);
-		return l;
-	}
-	
-	public void update(Object pojo) throws Exception {
-		getHibernateTemplate().update(pojo);
-	}
-	
-	public void delete(Object pojo) throws Exception {
-		getHibernateTemplate().delete(pojo);
-	}
-	
-	public void delete(Class<?> pojoClass, Serializable id) throws Exception {
-		Object o = this.load(pojoClass, id);
-		getHibernateTemplate().delete(o);
-	}
-	
+	@Override
 	public Object get(Class<?> pojoClass, Serializable id) {
 		if (id == null)
 			return null;
-		return getHibernateTemplate().get(pojoClass, id);
+		return this.getHibernateTemplate().get(pojoClass, id);
 	}
 	
+	@Override
+	public void save(Object pojo) throws Exception {
+		this.getHibernateTemplate().save(pojo);
+	}
+	
+	@Override
+	public void update(Object pojo) throws Exception {
+		this.getHibernateTemplate().update(pojo);
+	}
+	
+	@Override
 	public void insertOrUpdate(Object pojo) throws Exception {
 		this.getHibernateTemplate().saveOrUpdate(pojo);
 	}
 	
-	public List<?> find(String hql, Object[] param) {
-		return this.getHibernateTemplate().find(hql, param);
+	@Override
+	public void delete(Object pojo) throws Exception {
+		this.getHibernateTemplate().delete(pojo);
 	}
 	
-	public List<?> find(String hql, Object[] param, boolean userCache) {
-		this.getHibernateTemplate().setCacheQueries(userCache);
-		return this.getHibernateTemplate().find(hql, param);
+	@Override
+	public void delete(Class<?> pojoClass, Serializable id) throws Exception {
+		Object o = this.load(pojoClass, id);
+		this.getHibernateTemplate().delete(o);
 	}
 	
-	public List<?> find(final String queryString, final Object[] params, final int firstResult, final int maxResult) {
-		return this.find(queryString, params, firstResult, maxResult, false);
-	}
-	
-	@SuppressWarnings("rawtypes")
-	public List<?> find(final String queryString, final Object[] params, final int firstResult, final int maxResult, final boolean userCache) {
-		@SuppressWarnings("unchecked")
-		List list = (List) this.getHibernateTemplate().execute(new HibernateCallback() {
-			public Object doInHibernate(Session session) throws HibernateException, SQLException {
-				Query query = session.createQuery(queryString);
-				if (params != null && params.length > 0) {
-					for (int i = 0; i < params.length; i++) {
-						query.setParameter(i, params[i]);
-					}
-				}
-				query.setCacheable(userCache);
-				if (userCache) {
-				}
-				return query.setFirstResult((firstResult - 1) * maxResult).setMaxResults(maxResult).list();
-			}
-		});
-		return list;
-	}
-
-	public int executeByHql(final String hql) {
-		return this.getHibernateTemplate().bulkUpdate(hql);
-	}
-
-	public int executeByHql(final String hql, Object[] param) {
-		return this.getHibernateTemplate().bulkUpdate(hql, param);
-	}
-
-	public Iterator<?> iterate(String hql, Object[] objects) {
-		return this.getHibernateTemplate().iterate(hql, objects);
-	}
-
-	public Iterator<?> iterate(String hql) {
-		return this.getHibernateTemplate().iterate(hql);
-	}
-
-	public List<?> find(String hql) {
-		return this.getHibernateTemplate().find(hql);
-	}
-
+	@Override
 	public Object findOne(String hql, Object[] param){
 		List<?> list = this.getHibernateTemplate().find(hql, param);
 		if(list.size()!=1){
@@ -110,5 +56,70 @@ public class BaseDao extends HibernateDaoSupport implements IBaseDao {
 		}
 		return list.get(0);
 	}
+	
+	@Override
+	public List<?> find(String hql) {
+		return this.getHibernateTemplate().find(hql);
+	}
+	
+	@Override
+	public List<?> find(String hql, Object[] param) {
+		return this.getHibernateTemplate().find(hql, param);
+	}
+	
+	@Override
+	public List<?> find(String hql, Object[] param, boolean userCache) {
+		this.getHibernateTemplate().setCacheQueries(userCache);
+		return this.getHibernateTemplate().find(hql, param);
+	}
+	
+	@Override
+	public List<?> find(final String queryString, final Object[] params, final int firstResult, final int maxResult) {
+		return this.find(queryString, params, firstResult, maxResult, false);
+	}
+	
+	@Override
+	@SuppressWarnings("rawtypes")
+	public List<?> find(final String queryString, final Object[] params, final int firstResult, final int maxResult, final boolean userCache) {
+		List<?> list = (List<?>) this.getHibernateTemplate().execute((Session session)-> {
+			Query query = session.createQuery(queryString);
+			if (params != null && params.length > 0) {
+				for (int i = 0; i < params.length; i++) {
+					query.setParameter(i, params[i]);
+				}
+			}
+			query.setCacheable(userCache);
+			if (userCache) {
+			}
+			return query.setFirstResult((firstResult - 1) * maxResult).setMaxResults(maxResult).list();
+		});
+		return list;
+	}
 
+	@Override
+	public List<?> queryBySql(String sql) {
+		List<?> l = this.getHibernateTemplate().find(sql);
+		return l;
+	}
+
+	@Override
+	public int executeByHql(final String hql) {
+		return this.getHibernateTemplate().bulkUpdate(hql);
+	}
+
+	@Override
+	public int executeByHql(final String hql, Object[] param) {
+		return this.getHibernateTemplate().bulkUpdate(hql, param);
+	}
+
+	@Override
+	public Iterator<?> iterate(String hql) {
+		return this.getHibernateTemplate().iterate(hql);
+	}
+	
+	@Override
+	public Iterator<?> iterate(String hql, Object[] objects) {
+		return this.getHibernateTemplate().iterate(hql, objects);
+	}
+	
 }
